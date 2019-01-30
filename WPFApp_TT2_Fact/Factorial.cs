e7ddee0e70375369f92fdc13a2b3ebe3cb74eddf
km591192 <km591192@bk.ru>
2019-01-30 14:27:44 +0200

    Test project full

---
+++ WPFApp_TT2_Fact/Factorial.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFApp_TT2_Fact
+{
+    class Factorial
+    {
+        public string S { get; set; }
+        public int CheckN { get; set; }
+        private bool flag = false;
+
+        public void checkn(int N)
+        {
+            try
+            {
+                if (N >= 1 && N <= Fact(2000))
+                {
+                    S = "You enter number = " + N.ToString() + " is correct ";
+                    flag = true;
+                }
+                else
+                {
+                   S = "Input number is not correct";
+                   flag = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                S = ex.ToString();
+            }
+
+        }
+
+        
+        public void rev_Fact(int N)
+        {
+            int r = 1;
+            CheckN = -1;
+
+            for (int i = 1; i <= N; ++i)
+            {
+                r *= i;
+
+                if (N == r)
+                {
+                    CheckN = i;
+                    break;
+                }
+            }
+
+            if (CheckN != -1)
+            {
+                S = "Answer = " + CheckN.ToString();
+                flag = true;
+            }
+            else
+            {
+                S = "Answer is no found";
+                flag = false;
+            }
+        }
+
+        private double Fact(int N)
+        {
+            if (N == 1)
+                 return 1;
+            return N * Fact(N - 1);           
+        }
+
+        public void check_correct_answer (int N)
+        {
+            if (flag == true)
+            {
+                rev_Fact(N);
+                if (N == Fact(CheckN))
+                    S = "Answer = " + CheckN.ToString() + " \n" + N.ToString() + " = " + CheckN.ToString() + "!" + " (" + CheckN.ToString() + "! = " + Fact(CheckN) + ")";
+                else
+                    S = "Answer is no found";
+            }
+            else
+                S = "Answer is no found";
+
+        }
+
+       
+    }
+}
---
+++ WPFApp_TT2_Fact/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp_TT2_Fact
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Factorial factorial = new Factorial();
+
+        private int N = 0;
+        private string S = String.Empty;
+
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            N = Convert.ToInt32(tbNumber.Text);
+            factorial.checkn(N);
+            tbMsg.Text += factorial.S + "\n";
+            factorial.rev_Fact(N);
+            factorial.check_correct_answer(N);
+            tbMsg.Text += factorial.S + "\n";
+
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            tbNumber.Text = String.Empty;
+            tbMsg.Text = String.Empty;
+        }
+    }
+}
